6048b5c4dad30a91d606d39558d06f736b394536
changseoksong <a1373052011@gmail.com>
2023-05-12 11:04:00 +0900

    스토리북 프리뷰 설정

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import "../src/index.css";
+
 /** @type { import('@storybook/react').Preview } */
 const preview = {
   parameters: {
@@ -7,6 +9,17 @@ const preview = {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+  },
+};
+
+// parameters는 스토리북의 기능과 애드온의 동작을 제어하기 위하여 사용된다.
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
     },
   },
 };
